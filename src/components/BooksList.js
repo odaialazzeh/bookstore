859138771e94edf12b67650b87bc2681b3faf76f
odaialazzeh <odai.alazzeh@gmail.com>
2022-12-22 17:49:46 +0200

    Create BooksList.js

---
+++ src/components/BooksList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Bookitem from './BookItem';
+
+const Booklist = () => (
+  <div className="book">
+    <Bookitem />
+    <span id="line" />
+  </div>
+);
+
+export default Booklist;
