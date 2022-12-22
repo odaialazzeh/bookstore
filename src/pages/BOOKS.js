cb698191715070e2250e5e7de3e3b984273d74fb
odaialazzeh <odai.alazzeh@gmail.com>
2022-12-22 17:49:52 +0200

    Create BOOKS.js

---
+++ src/pages/BOOKS.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '../components/BookContainer';
+
+const Books = () => (
+  <div className="books-container">
+    <Container />
+  </div>
+);
+
+export default Books;
