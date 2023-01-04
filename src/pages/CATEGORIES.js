22bcdf8ed093b26f7644ee6cc3b25c416e85a1e7
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-04 13:22:44 +0200

    Update CATEGORIES.js

--- src/pages/CATEGORIES.js
+++ src/pages/CATEGORIES.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { statusChecker } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="categories">
-    <button type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handlebtn = (e) => {
+    e.preventDefault();
+    dispatch(statusChecker());
+  };
+  return (
+
+    <div className="categories">
+      <h2>{status}</h2>
+      <button type="button" onClick={handlebtn}>Check status</button>
+    </div>
+  );
+};
 
 export default Categories;
