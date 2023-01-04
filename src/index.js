7963ecafc57d8c65f7b8cf9ed71457bf49213e0a
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-04 13:22:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Books from './pages/BOOKS';
 import Categories from './pages/CATEGORIES';
+import store from './redux/configureStore';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route index element={<Books />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Books />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 );
