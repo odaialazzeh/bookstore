923023ab3a487e54652e4e31f088a18f8cceceb3
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-05 10:59:34 +0200

    Update BookItem.js

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -4,17 +4,25 @@ import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
 const Bookitem = ({ book }) => {
+  const {
+    id,
+    title,
+    author,
+    category,
+  } = book;
+
   const dispatch = useDispatch();
 
   const handleRemove = (e) => {
     e.preventDefault();
-    dispatch(removeBook(book.id));
+    dispatch(removeBook(id));
   };
 
   return (
     <div className="items">
-      <h2>{book.bookTitle}</h2>
-      <h3>{book.bookAuthor}</h3>
+      <h2>{category}</h2>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
       <div className="buttons">
         <button type="button">Comments</button>
         <span> | </span>
@@ -29,8 +37,9 @@ const Bookitem = ({ book }) => {
 Bookitem.propTypes = {
   book: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    bookTitle: PropTypes.string.isRequired,
-    bookAuthor: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
 };
 
