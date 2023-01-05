27c6540f38479179c91c6fa2c272ba3a9e4f06c5
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-05 10:59:42 +0200

    Update BooksList.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Bookitem from './BookItem';
+import { getBooks } from '../redux/books/books';
 
 const Booklist = () => {
-  const bookDetails = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const bookDetails = useSelector((state) => state.books.books);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <>
       <div className="book">
-        {bookDetails.map((book) => <Bookitem key={book.id} book={book} />)}
+        <h2 id="head">Books List</h2>
+        {bookDetails && bookDetails.map((book) => <Bookitem key={book.id} book={book} />)}
       </div>
       <div id="line" />
     </>
