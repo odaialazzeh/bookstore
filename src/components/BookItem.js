eae26a6ffc2cc04ff8b92b03b8fbe0000168c2ba
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-06 12:56:03 +0200

    Update BookItem.js

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { removeBook } from '../redux/books/books';
 
 const Bookitem = ({ book }) => {
@@ -10,8 +12,18 @@ const Bookitem = ({ book }) => {
     author,
     category,
   } = book;
-
   const dispatch = useDispatch();
+  const [percentage, setPercentage] = useState(0);
+
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter((count) => (count < 10 ? count + 1 : count));
+  };
+
+  const increasePercentage = () => {
+    setPercentage((count) => (count < 100 ? count + 10 : count));
+  };
 
   const handleRemove = (e) => {
     e.preventDefault();
@@ -19,16 +31,32 @@ const Bookitem = ({ book }) => {
   };
 
   return (
-    <div className="items">
-      <h2>{category}</h2>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <div className="buttons">
-        <button type="button">Comments</button>
-        <span> | </span>
-        <button type="button" onClick={handleRemove}>Remove</button>
-        <span> | </span>
-        <button type="button">Edit</button>
+    <div className="container">
+      <div className="items">
+        <h4>{category}</h4>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
+        <div className="buttons">
+          <button type="button">Comments</button>
+          <span> | </span>
+          <button type="button" onClick={handleRemove}>Remove</button>
+          <span> | </span>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div className="progress">
+        <div className="percentage">
+          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        </div>
+        <span className="VLine" />
+        <div>
+          <h4>CURRENT CHAPTER</h4>
+          <h3>
+            Chapter
+            {counter}
+          </h3>
+          <button type="button" onClick={() => { increase(); increasePercentage(); }}>UPDATE PROGRESS</button>
+        </div>
       </div>
     </div>
   );
